a05dab9aeb82e77896917a322aff222a42a4278a
Oluadun <97641195+Oluadun@users.noreply.github.com>
2022-03-12 15:53:11 -0600

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Stop all servers and start the server as a daemon
+# Stop all servers and start the server as a daemon 
 forever stopall
-#forever start --minUptime 10000 --spinSleepTime 1000 /home/ec2-user/nodejs/app.js
-sudo forever start -c "npm start" /home/ubuntu/myportfolio/ --minUptime 10000 --spinSleepTime 1000
-
+sudo forever --sourceDir /home/ubuntu/myportfolio -c "npm start"  --minUptime 10000 --spinSleepTime 1000
